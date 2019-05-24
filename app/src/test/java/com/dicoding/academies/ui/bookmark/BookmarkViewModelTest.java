9bb6d7e83016b4708ca612a9dabb435f685871e7
Gilang Ramadhan <gilangramadhan96.gr@gmail.com>
2019-05-24 10:51:18 +0700

    Add UnitTest

--- app/src/test/java/com/dicoding/academies/ui/bookmark/BookmarkViewModelTest.java
+++ app/src/test/java/com/dicoding/academies/ui/bookmark/BookmarkViewModelTest.java
@@ -1,17 +1,27 @@
 package com.dicoding.academies.ui.bookmark;
 
+import com.dicoding.academies.data.source.AcademyRepository;
+import com.dicoding.academies.data.source.local.entity.CourseEntity;
+import com.dicoding.academies.utils.DataDummy;
+import com.dicoding.academies.utils.FakeDataDummy;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class BookmarkViewModelTest {
     private BookmarkViewModel viewModel;
-
+    private AcademyRepository academyRepository = mock(AcademyRepository.class);
     @Before
     public void setUp() {
-        viewModel = new BookmarkViewModel();
+        viewModel = new BookmarkViewModel(academyRepository);
     }
 
     @After
@@ -20,6 +30,9 @@ public class BookmarkViewModelTest {
 
     @Test
     public void getBookmark() {
-        assertEquals(5, viewModel.getBookmarks().size());
+        when(academyRepository.getBookmarkedCourses()).thenReturn(FakeDataDummy.generateDummyCourses());
+        List<CourseEntity> resultCourse = viewModel.getBookmarks();
+        verify(academyRepository).getBookmarkedCourses();
+        assertEquals(5, resultCourse.size());
     }
 }

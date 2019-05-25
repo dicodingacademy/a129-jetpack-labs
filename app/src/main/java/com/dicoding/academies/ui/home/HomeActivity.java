972bd10206b78420db406692873763f34074a328
Gilang Ramadhan <gilangramadhan96.gr@gmail.com>
2019-05-25 21:27:02 +0700

    Update HomeActivity.java

--- app/src/main/java/com/dicoding/academies/ui/home/HomeActivity.java
+++ app/src/main/java/com/dicoding/academies/ui/home/HomeActivity.java
@@ -5,8 +5,12 @@ import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.dicoding.academies.R;
+import com.dicoding.academies.ui.academy.AcademyFragment;
+import com.dicoding.academies.ui.bookmark.BookmarkFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
@@ -14,22 +18,25 @@ public class HomeActivity extends AppCompatActivity {
     private final String SELECTED_MENU = "selected_menu";
     private BottomNavigationView navView;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.action_home:
-
-                    return true;
-                case R.id.action_bookmark:
-
-                    return true;
-            }
-            return false;
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = item -> {
+        Fragment fragment = null;
+        if (item.getItemId() == R.id.action_home) {
+            fragment = AcademyFragment.newInstance();
+        } else if (item.getItemId() == R.id.action_bookmark) {
+            fragment = BookmarkFragment.newInstance();
         }
+
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                    .replace(R.id.container, fragment)
+                    .commit();
+        }
+        return true;
     };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

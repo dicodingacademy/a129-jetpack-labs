82f9768e033cf3fbba664f73eb28204a62f1fcbb
Gilang Ramadhan <gilangramadhan96.gr@gmail.com>
2019-05-25 22:18:11 +0700

    Add Instrumental Test

---
+++ app/src/androidTest/java/com/dicoding/academies/ui/bookmark/BookmarkFragmentTest.java
@@ -0,0 +1,37 @@
+package com.dicoding.academies.ui.bookmark;
+
+import androidx.test.rule.ActivityTestRule;
+
+import com.dicoding.academies.R;
+import com.dicoding.academies.testing.SingleFragmentActivity;
+import com.dicoding.academies.utils.RecyclerViewItemCountAssertion;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.*;
+
+public class BookmarkFragmentTest {
+
+    @Rule
+    public ActivityTestRule<SingleFragmentActivity> activityRule = new ActivityTestRule<>(SingleFragmentActivity.class);
+    private BookmarkFragment bookmarkFragment = new BookmarkFragment();
+
+    @Before
+    public void setUp() {
+        activityRule.getActivity().setFragment(bookmarkFragment);
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void loadBookmarks() {
+        onView(withId(R.id.rv_bookmark)).check(new RecyclerViewItemCountAssertion(5));
+    }
+}
---
+++ app/src/debug/java/com/dicoding/academies/testing/SingleFragmentActivity.java
@@ -0,0 +1,44 @@
+package com.dicoding.academies.testing;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.dicoding.academies.R;
+
+public class SingleFragmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        View content = new FrameLayout(this);
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.WRAP_CONTENT,
+                Gravity.CENTER);
+        content.setLayoutParams(params);
+        content.setId(R.id.container);
+
+        setContentView(content);
+    }
+
+    public void setFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.container, fragment, "TEST")
+                .commit();
+    }
+
+    public void replaceFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.container, fragment)
+                .commit();
+    }
+}
+

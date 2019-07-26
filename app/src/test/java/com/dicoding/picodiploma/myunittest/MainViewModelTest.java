c59c035b104ee26a996f930febcbfb73de87c2d9
Gilang Ramadhan <gilangramadhan96.gr@gmail.com>
2019-07-26 16:15:39 +0700

    Update

--- app/src/test/java/com/dicoding/picodiploma/myunittest/MainViewModelTest.java
+++ app/src/test/java/com/dicoding/picodiploma/myunittest/MainViewModelTest.java
@@ -15,7 +15,6 @@ public class MainViewModelTest {
     private double dummyVolume = 504.0;
     private double dummyCircumference = 2016.0;
     private double dummySurfaceArea = 396.0;
-    private double dummyEmpty = 0.0;
     private double dummyLength = 12.0;
     private double dummyWidth = 7.0;
     private double dummyHeight = 6.0;
@@ -24,30 +23,6 @@ public class MainViewModelTest {
     public void before() {
         cuboidModel = mock(CuboidModel.class);
         mainViewModel = new MainViewModel(cuboidModel);
-    }
-
-    @Test
-    public void testMockVolume() {
-        when(mainViewModel.getVolume()).thenReturn(dummyVolume);
-        double volume = mainViewModel.getVolume();
-        verify(cuboidModel).getVolume();
-        assertEquals(dummyVolume, volume, 0.0001);
-    }
-
-    @Test
-    public void testMockCircumference() {
-        when(mainViewModel.getCircumference()).thenReturn(dummyCircumference);
-        double volume = mainViewModel.getCircumference();
-        verify(cuboidModel).getCircumference();
-        assertEquals(dummyCircumference, volume, 0.0001);
-    }
-
-    @Test
-    public void testMockSurfaceArea() {
-        when(mainViewModel.getSurfaceArea()).thenReturn(dummySurfaceArea);
-        double volume = mainViewModel.getSurfaceArea();
-        verify(cuboidModel).getSurfaceArea();
-        assertEquals(dummySurfaceArea, volume, 0.0001);
     }
 
     @Test
@@ -74,6 +49,30 @@ public class MainViewModelTest {
         mainViewModel = new MainViewModel(cuboidModel);
         mainViewModel.save(dummyWidth, dummyLength, dummyHeight);
         double volume = mainViewModel.getSurfaceArea();
+        assertEquals(dummySurfaceArea, volume, 0.0001);
+    }
+
+    @Test
+    public void testMockVolume() {
+        when(mainViewModel.getVolume()).thenReturn(dummyVolume);
+        double volume = mainViewModel.getVolume();
+        verify(cuboidModel).getVolume();
+        assertEquals(dummyVolume, volume, 0.0001);
+    }
+
+    @Test
+    public void testMockCircumference() {
+        when(mainViewModel.getCircumference()).thenReturn(dummyCircumference);
+        double volume = mainViewModel.getCircumference();
+        verify(cuboidModel).getCircumference();
+        assertEquals(dummyCircumference, volume, 0.0001);
+    }
+
+    @Test
+    public void testMockSurfaceArea() {
+        when(mainViewModel.getSurfaceArea()).thenReturn(dummySurfaceArea);
+        double volume = mainViewModel.getSurfaceArea();
+        verify(cuboidModel).getSurfaceArea();
         assertEquals(dummySurfaceArea, volume, 0.0001);
     }
 }

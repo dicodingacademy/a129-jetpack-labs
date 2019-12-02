a01101846acc1528dc81859652f158a78e1e727f
Arif <dicodingreviewer@gmail.com>
2019-12-02 17:05:44 +0700

    Update Kotlin Room

---
+++ Latihan/LatihanRoom/java/app/src/main/java/com/dicoding/picodiploma/mynoteapps/ui/main/MainActivity.java
@@ -0,0 +1,100 @@
+package com.dicoding.picodiploma.mynoteapps.ui.main;
+
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.View;
+
+import com.dicoding.picodiploma.mynoteapps.R;
+import com.dicoding.picodiploma.mynoteapps.database.Note;
+import com.dicoding.picodiploma.mynoteapps.helper.ViewModelFactory;
+import com.dicoding.picodiploma.mynoteapps.ui.insert.NoteAddUpdateActivity;
+
+import java.util.List;
+
+import static com.dicoding.picodiploma.mynoteapps.ui.insert.NoteAddUpdateActivity.REQUEST_UPDATE;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+
+    private NoteAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MainViewModel mainViewModel = obtainViewModel(MainActivity.this);
+        mainViewModel.getAllNotes().observe(this, noteObserver);
+
+        adapter = new NoteAdapter(MainActivity.this);
+
+        recyclerView = findViewById(R.id.rv_notes);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(adapter);
+
+        FloatingActionButton fabAdd = findViewById(R.id.fab_add);
+        fabAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (view.getId() == R.id.fab_add) {
+                    Intent intent = new Intent(MainActivity.this, NoteAddUpdateActivity.class);
+                    startActivityForResult(intent, NoteAddUpdateActivity.REQUEST_ADD);
+                }
+            }
+        });
+    }
+
+    @NonNull
+    private static MainViewModel obtainViewModel(AppCompatActivity activity) {
+        ViewModelFactory factory = ViewModelFactory.getInstance(activity.getApplication());
+
+        return new ViewModelProvider(activity, factory).get(MainViewModel.class);
+    }
+
+    private final Observer<List<Note>> noteObserver = new Observer<List<Note>>() {
+        @Override
+        public void onChanged(@Nullable List<Note> noteList) {
+            if (noteList != null) {
+                adapter.setListNotes(noteList);
+            }
+        }
+    };
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (data != null) {
+            if (requestCode == NoteAddUpdateActivity.REQUEST_ADD) {
+                if (resultCode == NoteAddUpdateActivity.RESULT_ADD) {
+                    showSnackbarMessage(getString(R.string.added));
+                }
+            } else if (requestCode == REQUEST_UPDATE) {
+                if (resultCode == NoteAddUpdateActivity.RESULT_UPDATE) {
+                    showSnackbarMessage(getString(R.string.changed));
+                } else if (resultCode == NoteAddUpdateActivity.RESULT_DELETE) {
+                    showSnackbarMessage(getString(R.string.deleted));
+                }
+            }
+        }
+    }
+
+    private void showSnackbarMessage(String message) {
+        Snackbar.make(recyclerView, message, Snackbar.LENGTH_SHORT).show();
+    }
+}

922ec0dd1a603e3fc76582018151f938be08382b
Dimas <dimas.maulanads@gmail.com>
2020-01-17 10:24:55 +0700

    Improve singleton

--- Project Academy/11. Pagination/kotlin/app/src/main/java/com/dicoding/academies/data/source/local/LocalDataSource.kt
+++ Project Academy/11. Pagination/kotlin/app/src/main/java/com/dicoding/academies/data/source/local/LocalDataSource.kt
@@ -14,10 +14,7 @@ class LocalDataSource private constructor(private val mAcademyDao: AcademyDao) {
         private var INSTANCE: LocalDataSource? = null
 
         fun getInstance(academyDao: AcademyDao): LocalDataSource {
-            if (INSTANCE == null) {
-                INSTANCE = LocalDataSource(academyDao)
-            }
-            return INSTANCE as LocalDataSource
+            return INSTANCE ?: LocalDataSource(academyDao)
         }
     }
 

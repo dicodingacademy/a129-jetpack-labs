af3b15d1c5b5ad4f5e308de03b6159975db16412
Alfian Yusfu Abdullah <alfianyabdullah@gmail.com>
2020-11-06 13:03:30 +0700

    [Kotlin] implement viewBinding

--- Latihan/LatihanInstrumentTest/kotlin/app/src/main/java/com/dicoding/picodiploma/myunittest/MainActivity.kt
+++ Latihan/LatihanInstrumentTest/kotlin/app/src/main/java/com/dicoding/picodiploma/myunittest/MainActivity.kt
@@ -1,55 +1,67 @@
 package com.dicoding.picodiploma.myunittest
 
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import com.dicoding.picodiploma.myunittest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
+    private lateinit var activityMainBinding: ActivityMainBinding
     private lateinit var mainViewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
         mainViewModel = MainViewModel(CuboidModel())
 
-        btn_save.setOnClickListener(this)
-        btn_calculate_surface_area.setOnClickListener(this)
-        btn_calculate_circumference.setOnClickListener(this)
-        btn_calculate_volume.setOnClickListener(this)
+        activityMainBinding.btnSave.setOnClickListener(this)
+        activityMainBinding.btnCalculateSurfaceArea.setOnClickListener(this)
+        activityMainBinding.btnCalculateCircumference.setOnClickListener(this)
+        activityMainBinding.btnCalculateVolume.setOnClickListener(this)
     }
 
     override fun onClick(v: View) {
-        val length = edt_length.text.toString().trim()
-        val width = edt_width.text.toString().trim()
-        val height = edt_height.text.toString().trim()
+        val length = activityMainBinding.edtLength.text.toString().trim()
+        val width = activityMainBinding.edtWidth.text.toString().trim()
+        val height = activityMainBinding.edtHeight.text.toString().trim()
 
         when {
-            length.isEmpty() -> edt_length.error = "Field ini tidak boleh kosong"
-            width.isEmpty() -> edt_width.error = "Field ini tidak boleh kosong"
-            height.isEmpty() -> edt_height.error = "Field ini tidak boleh kosong"
+            TextUtils.isEmpty(length) -> {
+                activityMainBinding.edtLength.error = "Field ini tidak boleh kosong"
+            }
+            TextUtils.isEmpty(width) -> {
+                activityMainBinding.edtWidth.error = "Field ini tidak boleh kosong"
+            }
+            TextUtils.isEmpty(height) -> {
+                activityMainBinding.edtHeight.error = "Field ini tidak boleh kosong"
+            }
+
             else -> {
-                val l = length.toDouble()
-                val w = width.toDouble()
-                val h = height.toDouble()
+                val valueLength = length.toDouble()
+                val valueWidth = width.toDouble()
+                val valueHeight = height.toDouble()
 
                 when (v.id) {
                     R.id.btn_save -> {
-                        mainViewModel.save(l, w, h)
+                        mainViewModel.save(valueLength, valueWidth, valueHeight)
                         visible()
                     }
                     R.id.btn_calculate_circumference -> {
-                        tv_result.text = mainViewModel.circumference.toString()
+                        activityMainBinding.tvResult.text = mainViewModel.circumference.toString()
                         gone()
                     }
                     R.id.btn_calculate_surface_area -> {
-                        tv_result.text = mainViewModel.surfaceArea.toString()
+                        activityMainBinding.tvResult.text = mainViewModel.surfaceArea.toString()
                         gone()
                     }
                     R.id.btn_calculate_volume -> {
-                        tv_result.text = mainViewModel.volume.toString()
+                        activityMainBinding.tvResult.text = mainViewModel.volume.toString()
                         gone()
                     }
                 }
@@ -58,16 +70,16 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun visible() {
-        btn_calculate_volume.visibility = View.VISIBLE
-        btn_calculate_circumference.visibility = View.VISIBLE
-        btn_calculate_surface_area.visibility = View.VISIBLE
-        btn_save.visibility = View.GONE
+        activityMainBinding.btnCalculateVolume.visibility = View.VISIBLE
+        activityMainBinding.btnCalculateCircumference.visibility = View.VISIBLE
+        activityMainBinding.btnCalculateSurfaceArea.visibility = View.VISIBLE
+        activityMainBinding.btnSave.visibility = View.GONE
     }
 
     private fun gone() {
-        btn_calculate_volume.visibility = View.GONE
-        btn_calculate_circumference.visibility = View.GONE
-        btn_calculate_surface_area.visibility = View.GONE
-        btn_save.visibility = View.VISIBLE
+        activityMainBinding.btnCalculateVolume.visibility = View.GONE
+        activityMainBinding.btnCalculateCircumference.visibility = View.GONE
+        activityMainBinding.btnCalculateSurfaceArea.visibility = View.GONE
+        activityMainBinding.btnSave.visibility = View.VISIBLE
     }
 }

654296ccce9c078f375d270d0b8c4dcce7ecb944
Alfian Yusuf Abdullah <alfianyabdullah@gmail.com>
2020-11-08 21:57:03 +0700

    [Kotlin] implement viewBinding

--- Latihan/LatihanViewModel/kotlin/app/src/main/java/com/dicoding/picodiploma/myviewmodel/MainActivity.kt
+++ Latihan/LatihanViewModel/kotlin/app/src/main/java/com/dicoding/picodiploma/myviewmodel/MainActivity.kt
@@ -3,48 +3,49 @@ package com.dicoding.picodiploma.myviewmodel
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import kotlinx.android.synthetic.main.activity_main.*
+import com.dicoding.picodiploma.myviewmodel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    // Membuat varibel global
+
+    private lateinit var activityMainBinding: ActivityMainBinding
     private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        // Menghubungkan ViewModel dengan Activity
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
-        // Menampilkan hasil pertama kali
+
         displayResult()
-        // Memberikan aksi klik kepada button calculate
-        btn_calculate.setOnClickListener {
-            // Mendapatkan string dari editText
-            val width = edt_width.text.toString()
-            val height = edt_height.text.toString()
-            val length = edt_length.text.toString()
-            // Melakukan pengecekan apakah empty atau tidak
+
+        activityMainBinding.btnCalculate.setOnClickListener {
+
+            val width = activityMainBinding.edtWidth.text.toString()
+            val height = activityMainBinding.edtHeight.text.toString()
+            val length = activityMainBinding.edtLength.text.toString()
+
             when {
                 width.isEmpty() -> {
-                    edt_width.error = "Masih kosong"
+                    activityMainBinding.edtWidth.error = "Masih kosong"
                 }
                 height.isEmpty() -> {
-                    edt_height.error = "Masih kosong"
+                    activityMainBinding.edtHeight.error = "Masih kosong"
                 }
                 length.isEmpty() -> {
-                    edt_length.error = "Masih kosong"
+                    activityMainBinding.edtLength.error = "Masih kosong"
                 }
                 else -> {
-                    // Melakukan pengiriman string ke ViewModel
+
                     viewModel.calculate(width, height, length)
-                    //Menampilkan hasil dari calculate
+
                     displayResult()
                 }
             }
         }
     }
 
-    // Menampilkan hasil dari ViewModel ke tvResults
     private fun displayResult() {
-        tv_result.text = viewModel.result.toString()
+        activityMainBinding.tvResult.text = viewModel.result.toString()
     }
 }

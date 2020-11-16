6324f193ae8cd5e1ad8ea2d438462b13bd349a5f
Alfian Yusuf Abdullah <alfianyabdullah@gmail.com>
2020-11-16 13:57:44 +0700

    [kotlin] changes deprecated component

--- Project Academy/03. ViewModel-test/kotlin/app/src/androidTest/java/com/dicoding/academies/ui/home/HomeActivityTest.kt
+++ Project Academy/03. ViewModel-test/kotlin/app/src/androidTest/java/com/dicoding/academies/ui/home/HomeActivityTest.kt
@@ -6,7 +6,7 @@ import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
-import androidx.test.rule.ActivityTestRule
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.dicoding.academies.R
 import com.dicoding.academies.utils.DataDummy
 import org.junit.Rule
@@ -17,7 +17,7 @@ class HomeActivityTest {
     private val dummyCourse = DataDummy.generateDummyCourses()
 
     @get:Rule
-    var activityRule = ActivityTestRule(HomeActivity::class.java)
+    var activityRule = ActivityScenarioRule(HomeActivity::class.java)
 
     @Test
     fun loadCourses() {

2bb8f445efac1c8570bd99acb6a566e28361a7d5
Alfian Yusuf Abdullah <alfianyabdullah@gmail.com>
2020-11-18 23:50:53 +0700

    [Kotlin] implement viewBinding

--- Latihan/LatihanLiveDataWithAPI/kotlin/app/src/main/java/com/dicoding/restaurantreview/MainActivity.kt
+++ Latihan/LatihanLiveDataWithAPI/kotlin/app/src/main/java/com/dicoding/restaurantreview/MainActivity.kt
@@ -7,41 +7,46 @@ import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.ArrayAdapter
 import androidx.activity.viewModels
-import androidx.lifecycle.Observer
+import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
-import kotlinx.android.synthetic.main.activity_main.*
+import com.dicoding.restaurantreview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private val mainViewModel : MainViewModel by viewModels()
+    private val mainViewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
 
         supportActionBar?.hide()
 
         mainViewModel.findRestaurant()
         mainViewModel.restaurant.observe(this, { restaurant ->
-            tvTitle.text = restaurant.name
-            tvDescription.text = "${restaurant.description.take(100)}..."
-            Glide.with(this).load("https://restaurant-api.dicoding.dev/images/large/${restaurant.pictureId}").into(ivPicture)
+            activityMainBinding.tvTitle.text = restaurant.name
+            activityMainBinding.tvDescription.text = "${restaurant.description.take(100)}..."
+            Glide.with(this)
+                .load("https://restaurant-api.dicoding.dev/images/large/${restaurant.pictureId}")
+                .into(activityMainBinding.ivPicture)
         })
 
-        mainViewModel.listReview.observe(this, { customerReviews ->
-            val listReview = ArrayList<String>()
-            customerReviews.forEach {
-                listReview.add("${it.review}\n- ${it.name}")
+        mainViewModel.listReview.observe(this, { consumerReviews ->
+            val listReview = consumerReviews.map {
+                "${it.review}\n- ${it.name}"
             }
-            lvReview.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listReview)
+
+            activityMainBinding.lvReview.adapter =
+                ArrayAdapter(this, R.layout.item_review, listReview)
         })
 
         mainViewModel.isLoading.observe(this, {
-            progressBar.visibility = if (it) View.VISIBLE else View.GONE
+            activityMainBinding.progressBar.visibility = if (it) View.VISIBLE else View.GONE
         })
 
-        btnSend.setOnClickListener { view ->
-            mainViewModel.postReview(edReview.text.toString())
+        activityMainBinding.btnSend.setOnClickListener { view ->
+            mainViewModel.postReview(activityMainBinding.edReview.text.toString())
             val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             imm.hideSoftInputFromWindow(view.windowToken, 0)
         }

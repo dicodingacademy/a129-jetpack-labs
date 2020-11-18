39d2bb438b4df1beb24f365f69c482677e4da2f5
Alfian Yusuf Abdullah <alfianyabdullah@gmail.com>
2020-11-18 21:51:48 +0700

    [Kotlin] sync with viewBinding implementation

--- Project Academy/05. Repository-test/kotlin/app/src/main/java/com/dicoding/academies/ui/detail/DetailCourseActivity.kt
+++ Project Academy/05. Repository-test/kotlin/app/src/main/java/com/dicoding/academies/ui/detail/DetailCourseActivity.kt
@@ -7,23 +7,32 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
 import com.dicoding.academies.R
 import com.dicoding.academies.data.source.local.entity.CourseEntity
+import com.dicoding.academies.databinding.ActivityDetailCourseBinding
+import com.dicoding.academies.databinding.ContentDetailCourseBinding
 import com.dicoding.academies.ui.reader.CourseReaderActivity
 import com.dicoding.academies.viewmodel.ViewModelFactory
-import kotlinx.android.synthetic.main.activity_detail_course.*
-import kotlinx.android.synthetic.main.content_detail_course.*
 
 class DetailCourseActivity : AppCompatActivity() {
+
     companion object {
         const val EXTRA_COURSE = "extra_course"
     }
 
+    private lateinit var detailContentBinding: ContentDetailCourseBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_course)
-        setSupportActionBar(toolbar)
+
+        val activityDetailCourseBinding = ActivityDetailCourseBinding.inflate(layoutInflater)
+        detailContentBinding = activityDetailCourseBinding.detailContent
+
+        setContentView(activityDetailCourseBinding.root)
+
+        setSupportActionBar(activityDetailCourseBinding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val adapter = DetailCourseAdapter()
@@ -42,31 +51,32 @@ class DetailCourseActivity : AppCompatActivity() {
             }
         }
 
-        rv_module.isNestedScrollingEnabled = false
-        rv_module.layoutManager = LinearLayoutManager(this)
-        rv_module.setHasFixedSize(true)
-        rv_module.adapter = adapter
-        val dividerItemDecoration = DividerItemDecoration(rv_module.context, DividerItemDecoration.VERTICAL)
-        rv_module.addItemDecoration(dividerItemDecoration)
+        with(detailContentBinding.rvModule) {
+            isNestedScrollingEnabled = false
+            layoutManager = LinearLayoutManager(this@DetailCourseActivity)
+            setHasFixedSize(true)
+            this.adapter = adapter
+            val dividerItemDecoration = DividerItemDecoration(this.context, DividerItemDecoration.VERTICAL)
+            addItemDecoration(dividerItemDecoration)
+        }
     }
 
     private fun populateCourse(courseEntity: CourseEntity) {
-        text_title.text = courseEntity.title
-        text_desc.text = courseEntity.description
-        text_date.text = resources.getString(R.string.deadline_date, courseEntity.deadline)
+        detailContentBinding.textTitle.text = courseEntity.title
+        detailContentBinding.textDescription.text = courseEntity.description
+        detailContentBinding.textDate.text = resources.getString(R.string.deadline_date, courseEntity.deadline)
 
         Glide.with(this)
                 .load(courseEntity.imagePath)
+                .transform(RoundedCorners(20))
                 .apply(RequestOptions.placeholderOf(R.drawable.ic_loading)
                         .error(R.drawable.ic_error))
-                .into(image_poster)
+                .into(detailContentBinding.imagePoster)
 
-        btn_start.setOnClickListener {
+        detailContentBinding.btnStart.setOnClickListener {
             val intent = Intent(this@DetailCourseActivity, CourseReaderActivity::class.java)
             intent.putExtra(CourseReaderActivity.EXTRA_COURSE_ID, courseEntity.courseId)
             startActivity(intent)
         }
     }
 }
-
-
--- Project Academy/05. Repository-test/kotlin/app/src/main/java/com/dicoding/academies/ui/reader/content/ModuleContentFragment.kt
+++ Project Academy/05. Repository-test/kotlin/app/src/main/java/com/dicoding/academies/ui/reader/content/ModuleContentFragment.kt
@@ -1,18 +1,15 @@
 package com.dicoding.academies.ui.reader.content
 
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.dicoding.academies.R
 import com.dicoding.academies.data.source.local.entity.ModuleEntity
+import com.dicoding.academies.databinding.FragmentModuleContentBinding
 import com.dicoding.academies.ui.reader.CourseReaderViewModel
 import com.dicoding.academies.viewmodel.ViewModelFactory
-import kotlinx.android.synthetic.main.fragment_module_content.*
-
 
 /**
  * A simple [Fragment] subclass.
@@ -20,19 +17,21 @@ import kotlinx.android.synthetic.main.fragment_module_content.*
 class ModuleContentFragment : Fragment() {
 
     companion object {
-        val TAG = ModuleContentFragment::class.java.simpleName
-
-        fun newInstance(): ModuleContentFragment {
-            return ModuleContentFragment()
-        }
+        val TAG: String = ModuleContentFragment::class.java.simpleName
+        fun newInstance(): ModuleContentFragment = ModuleContentFragment()
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_module_content, container, false)
+    private lateinit var fragmentModuleContentBinding: FragmentModuleContentBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        fragmentModuleContentBinding = FragmentModuleContentBinding.inflate(inflater, container, false)
+        return fragmentModuleContentBinding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         if (activity != null) {
             val factory = ViewModelFactory.getInstance(requireActivity())
             val viewModel = ViewModelProvider(requireActivity(), factory)[CourseReaderViewModel::class.java]
@@ -42,6 +41,6 @@ class ModuleContentFragment : Fragment() {
     }
 
     private fun populateWebView(module: ModuleEntity) {
-        web_view.loadData(module.contentEntity?.content, "text/html", "UTF-8")
+        fragmentModuleContentBinding.webView.loadData(module.contentEntity?.content ?: "", "text/html", "UTF-8")
     }
 }

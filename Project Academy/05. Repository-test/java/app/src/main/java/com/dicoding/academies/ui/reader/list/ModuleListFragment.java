0a0a32b7d9470ebef8d27b910ca99aa18249762f
Alfian Yusuf Abdullah <alfianyabdullah@gmail.com>
2020-11-18 22:08:34 +0700

    [Java] sync with viewBinding implementation

--- Project Academy/05. Repository-test/java/app/src/main/java/com/dicoding/academies/ui/reader/list/ModuleListFragment.java
+++ Project Academy/05. Repository-test/java/app/src/main/java/com/dicoding/academies/ui/reader/list/ModuleListFragment.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ProgressBar;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,10 +13,9 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.dicoding.academies.R;
 import com.dicoding.academies.data.source.local.entity.ModuleEntity;
+import com.dicoding.academies.databinding.FragmentModuleListBinding;
 import com.dicoding.academies.ui.reader.CourseReaderActivity;
 import com.dicoding.academies.ui.reader.CourseReaderCallback;
 import com.dicoding.academies.ui.reader.CourseReaderViewModel;
@@ -33,10 +31,10 @@ import java.util.List;
 public class ModuleListFragment extends Fragment implements MyAdapterClickListener {
 
     public static final String TAG = ModuleListFragment.class.getSimpleName();
+
+    private FragmentModuleListBinding fragmentModuleListBinding;
     private ModuleListAdapter adapter;
     private CourseReaderCallback courseReaderCallback;
-    private RecyclerView recyclerView;
-    private ProgressBar progressBar;
     private CourseReaderViewModel viewModel;
 
     public ModuleListFragment() {
@@ -49,28 +47,21 @@ public class ModuleListFragment extends Fragment implements MyAdapterClickListen
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_module_list, container, false);
+        fragmentModuleListBinding = FragmentModuleListBinding.inflate(inflater);
+        return fragmentModuleListBinding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        recyclerView = view.findViewById(R.id.rv_module);
-        progressBar = view.findViewById(R.id.progress_bar);
-    }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        if (getActivity() != null) {
-            ViewModelFactory factory = ViewModelFactory.getInstance(requireActivity());
-            viewModel = new ViewModelProvider(requireActivity(), factory).get(CourseReaderViewModel.class);
-            adapter = new ModuleListAdapter(this);
-            populateRecyclerView(viewModel.getModules());
-        }
+        ViewModelFactory factory = ViewModelFactory.getInstance(requireActivity());
+        viewModel = new ViewModelProvider(requireActivity(), factory).get(CourseReaderViewModel.class);
+        adapter = new ModuleListAdapter(this);
+        populateRecyclerView(viewModel.getModules());
     }
 
     @Override
@@ -86,14 +77,13 @@ public class ModuleListFragment extends Fragment implements MyAdapterClickListen
     }
 
     private void populateRecyclerView(List<ModuleEntity> modules) {
-        progressBar.setVisibility(View.GONE);
+        fragmentModuleListBinding.progressBar.setVisibility(View.GONE);
         adapter.setModules(modules);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setAdapter(adapter);
-        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(), DividerItemDecoration.VERTICAL);
-        recyclerView.addItemDecoration(dividerItemDecoration);
+        fragmentModuleListBinding.rvModule.setLayoutManager(new LinearLayoutManager(getContext()));
+        fragmentModuleListBinding.rvModule.setHasFixedSize(true);
+        fragmentModuleListBinding.rvModule.setAdapter(adapter);
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(fragmentModuleListBinding.rvModule.getContext(), DividerItemDecoration.VERTICAL);
+        fragmentModuleListBinding.rvModule.addItemDecoration(dividerItemDecoration);
     }
 
 }
-

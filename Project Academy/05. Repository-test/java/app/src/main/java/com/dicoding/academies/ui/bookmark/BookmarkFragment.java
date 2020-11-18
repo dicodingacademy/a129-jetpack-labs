615402ac762cc32a2e998f2a8dc5d07d5670e2f7
Alfian Yusuf Abdullah <alfianyabdullah@gmail.com>
2020-11-18 22:08:34 +0700

    [Java] sync with viewBinding implementation

--- Project Academy/05. Repository-test/java/app/src/main/java/com/dicoding/academies/ui/bookmark/BookmarkFragment.java
+++ Project Academy/05. Repository-test/java/app/src/main/java/com/dicoding/academies/ui/bookmark/BookmarkFragment.java
@@ -1,6 +1,5 @@
 package com.dicoding.academies.ui.bookmark;
 
-
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,39 +16,34 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.dicoding.academies.R;
 import com.dicoding.academies.data.source.local.entity.CourseEntity;
+import com.dicoding.academies.databinding.FragmentBookmarkBinding;
 import com.dicoding.academies.viewmodel.ViewModelFactory;
 
 import java.util.List;
-
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class BookmarkFragment extends Fragment implements BookmarkFragmentCallback {
-    private RecyclerView rvBookmark;
-    private ProgressBar progressBar;
+
+    private FragmentBookmarkBinding fragmentBookmarkBinding;
 
     public BookmarkFragment() {
         // Required empty public constructor
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bookmark, container, false);
+        fragmentBookmarkBinding = FragmentBookmarkBinding.inflate(inflater);
+        return fragmentBookmarkBinding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        rvBookmark = view.findViewById(R.id.rv_bookmark);
-        progressBar = view.findViewById(R.id.progress_bar);
-    }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
         if (getActivity() != null) {
             ViewModelFactory factory = ViewModelFactory.getInstance(getActivity());
             BookmarkViewModel viewModel = new ViewModelProvider(this, factory).get(BookmarkViewModel.class);
@@ -58,9 +52,9 @@ public class BookmarkFragment extends Fragment implements BookmarkFragmentCallba
             BookmarkAdapter adapter = new BookmarkAdapter(this);
             adapter.setCourses(courses);
 
-            rvBookmark.setLayoutManager(new LinearLayoutManager(getContext()));
-            rvBookmark.setHasFixedSize(true);
-            rvBookmark.setAdapter(adapter);
+            fragmentBookmarkBinding.rvBookmark.setLayoutManager(new LinearLayoutManager(getContext()));
+            fragmentBookmarkBinding.rvBookmark.setHasFixedSize(true);
+            fragmentBookmarkBinding.rvBookmark.setAdapter(adapter);
         }
     }
 
@@ -77,4 +71,3 @@ public class BookmarkFragment extends Fragment implements BookmarkFragmentCallba
         }
     }
 }
-
--- Project Academy/05. Repository-test/java/app/src/main/java/com/dicoding/academies/ui/reader/list/ModuleListAdapter.java
+++ Project Academy/05. Repository-test/java/app/src/main/java/com/dicoding/academies/ui/reader/list/ModuleListAdapter.java
@@ -10,6 +10,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.dicoding.academies.R;
 import com.dicoding.academies.data.source.local.entity.ModuleEntity;
+import com.dicoding.academies.databinding.ItemsModuleListCustomBinding;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,7 +37,8 @@ public class ModuleListAdapter extends RecyclerView.Adapter<ModuleListAdapter.Mo
     @NonNull
     @Override
     public ModuleViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new ModuleViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.items_module_list_custom, parent, false));
+        ItemsModuleListCustomBinding binding = ItemsModuleListCustomBinding.inflate(LayoutInflater.from(parent.getContext()));
+        return new ModuleViewHolder(binding);
     }
 
     @Override
@@ -53,19 +55,17 @@ public class ModuleListAdapter extends RecyclerView.Adapter<ModuleListAdapter.Mo
         return listModules.size();
     }
 
-    class ModuleViewHolder extends RecyclerView.ViewHolder {
-        final TextView textTitle;
-        final TextView textLastSeen;
+    static class ModuleViewHolder extends RecyclerView.ViewHolder {
+        final ItemsModuleListCustomBinding binding;
 
-        ModuleViewHolder(View itemView) {
-            super(itemView);
-            textTitle = itemView.findViewById(R.id.text_module_title);
-            textLastSeen = itemView.findViewById(R.id.text_last_seen);
+        ModuleViewHolder(ItemsModuleListCustomBinding binding) {
+            super(binding.getRoot());
+
+            this.binding = binding;
         }
 
         void bind(ModuleEntity module) {
-            textTitle.setText(module.getTitle());
+            binding.textModuleTitle.setText(module.getTitle());
         }
     }
 }
-

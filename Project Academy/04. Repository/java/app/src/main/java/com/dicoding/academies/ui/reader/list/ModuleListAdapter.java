067d8c1f612796fb00c3114786cb421c1343e688
Alfian Yusuf Abdullah <alfianyabdullah@gmail.com>
2020-11-18 14:51:59 +0700

    [Java] sync with viewBinding implementation

--- Project Academy/04. Repository/java/app/src/main/java/com/dicoding/academies/ui/reader/list/ModuleListAdapter.java
+++ Project Academy/04. Repository/java/app/src/main/java/com/dicoding/academies/ui/reader/list/ModuleListAdapter.java
@@ -1,15 +1,13 @@
 package com.dicoding.academies.ui.reader.list;
 
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.dicoding.academies.R;
 import com.dicoding.academies.data.source.local.entity.ModuleEntity;
+import com.dicoding.academies.databinding.ItemsModuleListCustomBinding;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,7 +34,8 @@ public class ModuleListAdapter extends RecyclerView.Adapter<ModuleListAdapter.Mo
     @NonNull
     @Override
     public ModuleViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new ModuleViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.items_module_list_custom, parent, false));
+        ItemsModuleListCustomBinding binding = ItemsModuleListCustomBinding.inflate(LayoutInflater.from(parent.getContext()));
+        return new ModuleViewHolder(binding);
     }
 
     @Override
@@ -53,18 +52,17 @@ public class ModuleListAdapter extends RecyclerView.Adapter<ModuleListAdapter.Mo
         return listModules.size();
     }
 
-    class ModuleViewHolder extends RecyclerView.ViewHolder {
-        final TextView textTitle;
-        final TextView textLastSeen;
+    static class ModuleViewHolder extends RecyclerView.ViewHolder {
+        final ItemsModuleListCustomBinding binding;
 
-        ModuleViewHolder(View itemView) {
-            super(itemView);
-            textTitle = itemView.findViewById(R.id.text_module_title);
-            textLastSeen = itemView.findViewById(R.id.text_last_seen);
+        ModuleViewHolder(ItemsModuleListCustomBinding binding) {
+            super(binding.getRoot());
+
+            this.binding = binding;
         }
 
         void bind(ModuleEntity module) {
-            textTitle.setText(module.getTitle());
+            binding.textModuleTitle.setText(module.getTitle());
         }
     }
 }

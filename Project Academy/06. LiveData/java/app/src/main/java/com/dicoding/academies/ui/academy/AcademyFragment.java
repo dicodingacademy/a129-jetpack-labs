5c27a4da9d63fd7f6872513207856153f4fc62ae
Alfian Yusuf Abdullah <alfianyabdullah@gmail.com>
2020-11-19 17:09:00 +0700

    [Java] sync with viewBinding implementation

--- Project Academy/06. LiveData/java/app/src/main/java/com/dicoding/academies/ui/academy/AcademyFragment.java
+++ Project Academy/06. LiveData/java/app/src/main/java/com/dicoding/academies/ui/academy/AcademyFragment.java
@@ -1,46 +1,36 @@
 package com.dicoding.academies.ui.academy;
 
-
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ProgressBar;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.dicoding.academies.R;
+import com.dicoding.academies.databinding.FragmentAcademyBinding;
 import com.dicoding.academies.viewmodel.ViewModelFactory;
-
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class AcademyFragment extends Fragment {
-    private RecyclerView rvCourse;
-    private ProgressBar progressBar;
+
+    private FragmentAcademyBinding fragmentAcademyBinding;
 
     public AcademyFragment() {
         // Required empty public constructor
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_academy, container, false);
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        rvCourse = view.findViewById(R.id.rv_academy);
-        progressBar = view.findViewById(R.id.progress_bar);
+        fragmentAcademyBinding = FragmentAcademyBinding.inflate(inflater);
+        return fragmentAcademyBinding.getRoot();
     }
 
     @Override
@@ -51,17 +41,15 @@ public class AcademyFragment extends Fragment {
             AcademyViewModel viewModel = new ViewModelProvider(this, factory).get(AcademyViewModel.class);
 
             AcademyAdapter academyAdapter = new AcademyAdapter();
-            progressBar.setVisibility(View.VISIBLE);
             viewModel.getCourses().observe(this, courses -> {
-                        progressBar.setVisibility(View.GONE);
                         academyAdapter.setCourses(courses);
                         academyAdapter.notifyDataSetChanged();
                     }
             );
 
-            rvCourse.setLayoutManager(new LinearLayoutManager(getContext()));
-            rvCourse.setHasFixedSize(true);
-            rvCourse.setAdapter(academyAdapter);
+            fragmentAcademyBinding.rvAcademy.setLayoutManager(new LinearLayoutManager(getContext()));
+            fragmentAcademyBinding.rvAcademy.setHasFixedSize(true);
+            fragmentAcademyBinding.rvAcademy.setAdapter(academyAdapter);
         }
     }
 }
--- Project Academy/06. LiveData/java/app/src/main/java/com/dicoding/academies/ui/detail/DetailCourseActivity.java
+++ Project Academy/06. LiveData/java/app/src/main/java/com/dicoding/academies/ui/detail/DetailCourseActivity.java
@@ -2,53 +2,39 @@ package com.dicoding.academies.ui.detail;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.dicoding.academies.R;
 import com.dicoding.academies.data.source.local.entity.CourseEntity;
+import com.dicoding.academies.databinding.ActivityDetailCourseBinding;
+import com.dicoding.academies.databinding.ContentDetailCourseBinding;
 import com.dicoding.academies.ui.reader.CourseReaderActivity;
 import com.dicoding.academies.viewmodel.ViewModelFactory;
 
 public class DetailCourseActivity extends AppCompatActivity {
 
     public static final String EXTRA_COURSE = "extra_course";
-    private Button btnStart;
-    private TextView textTitle;
-    private TextView textDesc;
-    private TextView textDate;
-    private ImageView imagePoster;
-    private ProgressBar progressBar;
+    private ContentDetailCourseBinding contentDetailCourseBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail_course);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        ActivityDetailCourseBinding activityDetailCourseBinding = ActivityDetailCourseBinding.inflate(getLayoutInflater());
+        contentDetailCourseBinding = activityDetailCourseBinding.detailContent;
+
+        setContentView(activityDetailCourseBinding.getRoot());
+
+        setSupportActionBar(activityDetailCourseBinding.toolbar);
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
-
-        btnStart = findViewById(R.id.btn_start);
-        textTitle = findViewById(R.id.text_title);
-        textDesc = findViewById(R.id.text_description);
-        textDate = findViewById(R.id.text_date);
-        RecyclerView rvModule = findViewById(R.id.rv_module);
-        imagePoster = findViewById(R.id.image_poster);
-        progressBar = findViewById(R.id.progress_bar);
 
         DetailCourseAdapter adapter = new DetailCourseAdapter();
         ViewModelFactory factory = ViewModelFactory.getInstance(this);
@@ -60,9 +46,7 @@ public class DetailCourseActivity extends AppCompatActivity {
             if (courseId != null) {
                 viewModel.setSelectedCourse(courseId);
 
-                progressBar.setVisibility(View.VISIBLE);
                 viewModel.getModules().observe(this, modules -> {
-                    progressBar.setVisibility(View.GONE);
                     adapter.setModules(modules);
                     adapter.notifyDataSetChanged();
                 });
@@ -70,26 +54,26 @@ public class DetailCourseActivity extends AppCompatActivity {
             }
         }
 
-        rvModule.setNestedScrollingEnabled(false);
-        rvModule.setLayoutManager(new LinearLayoutManager(this));
-        rvModule.setHasFixedSize(true);
-        rvModule.setAdapter(adapter);
-        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(rvModule.getContext(), DividerItemDecoration.VERTICAL);
-        rvModule.addItemDecoration(dividerItemDecoration);
+        contentDetailCourseBinding.rvModule.setNestedScrollingEnabled(false);
+        contentDetailCourseBinding.rvModule.setLayoutManager(new LinearLayoutManager(this));
+        contentDetailCourseBinding.rvModule.setHasFixedSize(true);
+        contentDetailCourseBinding.rvModule.setAdapter(adapter);
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(contentDetailCourseBinding.rvModule.getContext(), DividerItemDecoration.VERTICAL);
+        contentDetailCourseBinding.rvModule.addItemDecoration(dividerItemDecoration);
     }
 
     private void populateCourse(CourseEntity courseEntity) {
-        textTitle.setText(courseEntity.getTitle());
-        textDesc.setText(courseEntity.getDescription());
-        textDate.setText(String.format("Deadline %s", courseEntity.getDeadline()));
+        contentDetailCourseBinding.textTitle.setText(courseEntity.getTitle());
+        contentDetailCourseBinding.textDescription.setText(courseEntity.getDescription());
+        contentDetailCourseBinding.textDate.setText(String.format("Deadline %s", courseEntity.getDeadline()));
 
         Glide.with(this)
                 .load(courseEntity.getImagePath())
                 .apply(RequestOptions.placeholderOf(R.drawable.ic_loading)
                         .error(R.drawable.ic_error))
-                .into(imagePoster);
+                .into(contentDetailCourseBinding.imagePoster);
 
-        btnStart.setOnClickListener(v -> {
+        contentDetailCourseBinding.btnStart.setOnClickListener(v -> {
             Intent intent = new Intent(DetailCourseActivity.this, CourseReaderActivity.class);
             intent.putExtra(CourseReaderActivity.EXTRA_COURSE_ID, courseEntity.getCourseId());
             startActivity(intent);

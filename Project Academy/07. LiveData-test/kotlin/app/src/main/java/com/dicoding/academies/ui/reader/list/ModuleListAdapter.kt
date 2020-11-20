0294916507f7c011ec705398db7895fe28a0149a
Alfian Yusuf Abdullah <alfianyabdullah@gmail.com>
2020-11-20 15:48:56 +0700

    [Kotlin] sync with viewBinding implementation

--- Project Academy/07. LiveData-test/kotlin/app/src/main/java/com/dicoding/academies/ui/reader/list/ModuleListAdapter.kt
+++ Project Academy/07. LiveData-test/kotlin/app/src/main/java/com/dicoding/academies/ui/reader/list/ModuleListAdapter.kt
@@ -1,12 +1,10 @@
 package com.dicoding.academies.ui.reader.list
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.dicoding.academies.R
 import com.dicoding.academies.data.source.local.entity.ModuleEntity
+import com.dicoding.academies.databinding.ItemsModuleListCustomBinding
 import java.util.*
 
 class ModuleListAdapter internal constructor(private val listener: MyAdapterClickListener) : RecyclerView.Adapter<ModuleListAdapter.ModuleViewHolder>() {
@@ -19,8 +17,8 @@ class ModuleListAdapter internal constructor(private val listener: MyAdapterClic
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ModuleViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.items_module_list_custom, parent, false)
-        return ModuleViewHolder(view)
+        val binding = ItemsModuleListCustomBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ModuleViewHolder(binding)
     }
 
     override fun onBindViewHolder(viewHolder: ModuleViewHolder, position: Int) {
@@ -33,10 +31,9 @@ class ModuleListAdapter internal constructor(private val listener: MyAdapterClic
 
     override fun getItemCount(): Int = listModules.size
 
-    inner class ModuleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val textTitle: TextView = itemView.findViewById(R.id.text_module_title)
+    inner class ModuleViewHolder(private val binding: ItemsModuleListCustomBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(module: ModuleEntity) {
-            textTitle.text = module.title
+            binding.textModuleTitle.text = module.title
         }
     }
 }
@@ -44,4 +41,3 @@ class ModuleListAdapter internal constructor(private val listener: MyAdapterClic
 internal interface MyAdapterClickListener {
     fun onItemClicked(position: Int, moduleId: String)
 }
-

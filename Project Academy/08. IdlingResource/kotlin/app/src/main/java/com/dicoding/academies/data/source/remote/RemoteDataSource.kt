df392088d7bc369d8f4cf42dd2206cb21084a575
Alfian Yusuf Abdullah <alfianyabdullah@gmail.com>
2020-11-26 13:34:13 +0700

    [Kotlin] resolve deprecated component

--- Project Academy/08. IdlingResource/kotlin/app/src/main/java/com/dicoding/academies/data/source/remote/RemoteDataSource.kt
+++ Project Academy/08. IdlingResource/kotlin/app/src/main/java/com/dicoding/academies/data/source/remote/RemoteDataSource.kt
@@ -1,6 +1,7 @@
 package com.dicoding.academies.data.source.remote
 
 import android.os.Handler
+import android.os.Looper
 import com.dicoding.academies.data.source.remote.response.ContentResponse
 import com.dicoding.academies.data.source.remote.response.CourseResponse
 import com.dicoding.academies.data.source.remote.response.ModuleResponse

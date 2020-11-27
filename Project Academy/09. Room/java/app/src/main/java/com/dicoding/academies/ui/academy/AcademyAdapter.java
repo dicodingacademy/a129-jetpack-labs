32a2f105d805ff46c8f55a33998bc06f3cf3bce4
Alfian Yusuf Abdullah <alfianyabdullah@gmail.com>
2020-11-27 14:10:23 +0700

    [java] remove unused import

--- Project Academy/09. Room/java/app/src/main/java/com/dicoding/academies/ui/academy/AcademyAdapter.java
+++ Project Academy/09. Room/java/app/src/main/java/com/dicoding/academies/ui/academy/AcademyAdapter.java
@@ -2,10 +2,7 @@ package com.dicoding.academies.ui.academy;
 
 import android.content.Intent;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;

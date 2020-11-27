9831a2a40120e7531f6938398af2b919399f2de4
Alfian Yusuf Abdullah <alfianyabdullah@gmail.com>
2020-11-27 16:20:58 +0700

    [kotlin] sync with viewBinding implementation

--- Project Academy/12. Pagination-test/kotlin/app/src/main/java/com/dicoding/academies/ui/academy/AcademyAdapter.kt
+++ Project Academy/12. Pagination-test/kotlin/app/src/main/java/com/dicoding/academies/ui/academy/AcademyAdapter.kt
@@ -1,9 +1,7 @@
 package com.dicoding.academies.ui.academy
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.paging.PagedListAdapter
 import androidx.recyclerview.widget.DiffUtil
@@ -12,17 +10,10 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.dicoding.academies.R
 import com.dicoding.academies.data.source.local.entity.CourseEntity
+import com.dicoding.academies.databinding.ItemsAcademyBinding
 import com.dicoding.academies.ui.detail.DetailCourseActivity
-import kotlinx.android.synthetic.main.items_academy.view.*
 
-class AcademyAdapter internal constructor() : PagedListAdapter<CourseEntity, AcademyAdapter.CourseViewHolder>(DIFF_CALLBACK) {
-//    private var listCourses = ArrayList<CourseEntity>()
-//
-//    fun setCourses(courses: List<CourseEntity>?) {
-//        if (courses == null) return
-//        this.listCourses.clear()
-//        this.listCourses.addAll(courses)
-//    }
+class AcademyAdapter : PagedListAdapter<CourseEntity, AcademyAdapter.CourseViewHolder>(DIFF_CALLBACK) {
 
     companion object {
         private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<CourseEntity>() {
@@ -30,7 +21,6 @@ class AcademyAdapter internal constructor() : PagedListAdapter<CourseEntity, Aca
                 return oldItem.courseId == newItem.courseId
             }
 
-            @SuppressLint("DiffUtilEquals")
             override fun areContentsTheSame(oldItem: CourseEntity, newItem: CourseEntity): Boolean {
                 return oldItem == newItem
             }
@@ -38,23 +28,22 @@ class AcademyAdapter internal constructor() : PagedListAdapter<CourseEntity, Aca
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CourseViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.items_academy, parent, false)
-        return CourseViewHolder(view)
+        val itemsAcademyBinding = ItemsAcademyBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return CourseViewHolder(itemsAcademyBinding)
     }
 
     override fun onBindViewHolder(holder: CourseViewHolder, position: Int) {
-        val course = getItem(position) as CourseEntity
-        holder.bind(course)
+        val course = getItem(position)
+        if (course != null) {
+            holder.bind(course)
+        }
     }
 
-//    override fun getItemCount(): Int = listCourses.size
-
-    class CourseViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class CourseViewHolder(private val binding: ItemsAcademyBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(course: CourseEntity) {
-            with(itemView) {
-                tv_item_title.text = course.title
-                tv_item_description.text = course.description
-                tv_item_date.text = itemView.resources.getString(R.string.deadline_date, course.deadline)
+            with(binding) {
+                tvItemTitle.text = course.title
+                tvItemDate.text = itemView.resources.getString(R.string.deadline_date, course.deadline)
                 itemView.setOnClickListener {
                     val intent = Intent(itemView.context, DetailCourseActivity::class.java)
                     intent.putExtra(DetailCourseActivity.EXTRA_COURSE, course.courseId)
@@ -64,9 +53,8 @@ class AcademyAdapter internal constructor() : PagedListAdapter<CourseEntity, Aca
                         .load(course.imagePath)
                         .apply(RequestOptions.placeholderOf(R.drawable.ic_loading)
                                 .error(R.drawable.ic_error))
-                        .into(img_poster)
+                        .into(imgPoster)
             }
         }
     }
 }
-
--- Project Academy/12. Pagination-test/kotlin/app/src/main/java/com/dicoding/academies/ui/bookmark/BookmarkAdapter.kt
+++ Project Academy/12. Pagination-test/kotlin/app/src/main/java/com/dicoding/academies/ui/bookmark/BookmarkAdapter.kt
@@ -1,9 +1,7 @@
 package com.dicoding.academies.ui.bookmark
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.paging.PagedListAdapter
 import androidx.recyclerview.widget.DiffUtil
@@ -12,12 +10,8 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.dicoding.academies.R
 import com.dicoding.academies.data.source.local.entity.CourseEntity
+import com.dicoding.academies.databinding.ItemsBookmarkBinding
 import com.dicoding.academies.ui.detail.DetailCourseActivity
-import kotlinx.android.synthetic.main.items_academy.view.img_poster
-import kotlinx.android.synthetic.main.items_academy.view.tv_item_date
-import kotlinx.android.synthetic.main.items_academy.view.tv_item_description
-import kotlinx.android.synthetic.main.items_academy.view.tv_item_title
-import kotlinx.android.synthetic.main.items_bookmark.view.*
 
 class BookmarkAdapter(private val callback: BookmarkFragmentCallback) : PagedListAdapter<CourseEntity, BookmarkAdapter.CourseViewHolder>(DIFF_CALLBACK) {
     companion object {
@@ -26,55 +20,43 @@ class BookmarkAdapter(private val callback: BookmarkFragmentCallback) : PagedLis
                 return oldItem.courseId == newItem.courseId
             }
 
-            @SuppressLint("DiffUtilEquals")
             override fun areContentsTheSame(oldItem: CourseEntity, newItem: CourseEntity): Boolean {
                 return oldItem == newItem
             }
         }
     }
-//    hapus kode di bawah ini
-//    private val listCourses = ArrayList<CourseEntity>()
-//
-//    fun setCourses(courses: List<CourseEntity>?) {
-//        if (courses == null) return
-//        this.listCourses.clear()
-//        this.listCourses.addAll(courses)
-//    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CourseViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.items_bookmark, parent, false)
-        return CourseViewHolder(view)
+        val itemsBookmarkBinding = ItemsBookmarkBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return CourseViewHolder(itemsBookmarkBinding)
     }
 
     override fun onBindViewHolder(holder: CourseViewHolder, position: Int) {
-        val course = getItem(position) as CourseEntity
-        holder.bind(course)
+        val course = getItem(position)
+        if (course != null) {
+            holder.bind(course)
+        }
     }
 
-    fun getSwipedData(swipedPosition: Int): CourseEntity = getItem(swipedPosition) as CourseEntity
+    fun getSwipedData(swipedPosition: Int): CourseEntity? = getItem(swipedPosition)
 
-//    hapus kode di bawah ini
-//    override fun getItemCount(): Int = listCourses.size
-
-    inner class CourseViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind(course: CourseEntity){
-            with(itemView){
-                tv_item_title.text = course.title
-                tv_item_description.text = course.description
-                tv_item_date.text = itemView.resources.getString(R.string.deadline_date, course.deadline)
+    inner class CourseViewHolder(private val binding: ItemsBookmarkBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(course: CourseEntity) {
+            with(binding) {
+                tvItemTitle.text = course.title
+                tvItemDate.text = itemView.resources.getString(R.string.deadline_date, course.deadline)
                 itemView.setOnClickListener {
                     val intent = Intent(itemView.context, DetailCourseActivity::class.java)
                     intent.putExtra(DetailCourseActivity.EXTRA_COURSE, course.courseId)
                     itemView.context.startActivity(intent)
                 }
-                img_share.setOnClickListener { callback.onShareClick(course) }
+                imgShare.setOnClickListener { callback.onShareClick(course) }
                 Glide.with(itemView.context)
                         .load(course.imagePath)
                         .apply(RequestOptions.placeholderOf(R.drawable.ic_loading)
                                 .error(R.drawable.ic_error))
-                        .into(img_poster)
+                        .into(imgPoster)
             }
         }
     }
 }
-

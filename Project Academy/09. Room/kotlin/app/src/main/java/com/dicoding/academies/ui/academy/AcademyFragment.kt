b9e470366242781aae0597469327ed681b9a3820
Alfian Yusuf Abdullah <alfianyabdullah@gmail.com>
2020-11-27 13:57:46 +0700

    [kotlin] sync with viewBinding implementation

--- Project Academy/09. Room/kotlin/app/src/main/java/com/dicoding/academies/ui/academy/AcademyFragment.kt
+++ Project Academy/09. Room/kotlin/app/src/main/java/com/dicoding/academies/ui/academy/AcademyFragment.kt
@@ -1,63 +1,67 @@
 package com.dicoding.academies.ui.academy
 
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.dicoding.academies.R
+import com.dicoding.academies.databinding.FragmentAcademyBinding
 import com.dicoding.academies.viewmodel.ViewModelFactory
 import com.dicoding.academies.vo.Status
-import kotlinx.android.synthetic.main.fragment_academy.*
-
 
 /**
  * A simple [Fragment] subclass.
  */
 class AcademyFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_academy, container, false)
+    private var _fragmentAcademyBinding: FragmentAcademyBinding? = null
+    private val binding get() = _fragmentAcademyBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        _fragmentAcademyBinding = FragmentAcademyBinding.inflate(layoutInflater, container, false)
+        return binding?.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         if (activity != null) {
 
             val factory = ViewModelFactory.getInstance(requireActivity())
             val viewModel = ViewModelProvider(this, factory)[AcademyViewModel::class.java]
 
             val academyAdapter = AcademyAdapter()
-            viewModel.getCourses().observe(this, Observer { courses ->
+            viewModel.getCourses().observe(this, { courses ->
                 if (courses != null) {
                     when (courses.status) {
-                        Status.LOADING -> progress_bar.visibility = View.VISIBLE
+                        Status.LOADING -> binding?.progressBar?.visibility = View.VISIBLE
                         Status.SUCCESS -> {
-                            progress_bar.visibility = View.GONE
+                            binding?.progressBar?.visibility = View.GONE
                             academyAdapter.setCourses(courses.data)
                             academyAdapter.notifyDataSetChanged()
                         }
                         Status.ERROR -> {
-                            progress_bar.visibility = View.GONE
+                            binding?.progressBar?.visibility = View.GONE
                             Toast.makeText(context, "Terjadi kesalahan", Toast.LENGTH_SHORT).show()
                         }
                     }
                 }
             })
 
-            with(rv_academy) {
-                layoutManager = LinearLayoutManager(context)
-                setHasFixedSize(true)
-                adapter = academyAdapter
+            with(binding?.rvAcademy) {
+                this?.layoutManager = LinearLayoutManager(context)
+                this?.setHasFixedSize(true)
+                this?.adapter = academyAdapter
             }
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _fragmentAcademyBinding = null
     }
 }
 

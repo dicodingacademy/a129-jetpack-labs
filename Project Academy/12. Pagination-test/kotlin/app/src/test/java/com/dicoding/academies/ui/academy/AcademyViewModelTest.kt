b5f2fc32bf19ed2ef13a1752513c78982d74e398
Alfian Yusuf Abdullah <alfianyabdullah@gmail.com>
2021-01-04 16:58:14 +0800

    [java] changes testing

--- Project Academy/12. Pagination-test/kotlin/app/src/test/java/com/dicoding/academies/ui/academy/AcademyViewModelTest.kt
+++ Project Academy/12. Pagination-test/kotlin/app/src/test/java/com/dicoding/academies/ui/academy/AcademyViewModelTest.kt
@@ -4,18 +4,22 @@ import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.paging.PagedList
+import androidx.paging.PositionalDataSource
 import com.dicoding.academies.data.AcademyRepository
 import com.dicoding.academies.data.source.local.entity.CourseEntity
+import com.dicoding.academies.utils.DataDummy
 import com.dicoding.academies.vo.Resource
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
-import org.mockito.Mockito.*
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.verify
 import org.mockito.junit.MockitoJUnitRunner
+import java.util.concurrent.Executors
 
 @RunWith(MockitoJUnitRunner::class)
 class AcademyViewModelTest {
@@ -31,28 +35,77 @@ class AcademyViewModelTest {
     @Mock
     private lateinit var observer: Observer<Resource<PagedList<CourseEntity>>>
 
-    @Mock
-    private lateinit var pagedList: PagedList<CourseEntity>
-
     @Before
     fun setUp() {
         viewModel = AcademyViewModel(academyRepository)
     }
 
     @Test
-    fun getCourses() {
-        val dummyCourses = Resource.success(pagedList)
-        `when`(dummyCourses.data?.size).thenReturn(5)
-        val courses = MutableLiveData<Resource<PagedList<CourseEntity>>>()
-        courses.value = dummyCourses
+    fun `getCourses should be success`() {
+        val courses = PagedTestDataSources.snapshot(DataDummy.generateDummyCourses())
+        val expected = MutableLiveData<Resource<PagedList<CourseEntity>>>()
+        expected.value = Resource.success(courses)
 
-        `when`(academyRepository.getAllCourses()).thenReturn(courses)
-        val courseEntities = viewModel.getCourses().value?.data
-        verify(academyRepository).getAllCourses()
-        assertNotNull(courseEntities)
-        assertEquals(5, courseEntities?.size)
+        `when`(academyRepository.getAllCourses()).thenReturn(expected)
 
         viewModel.getCourses().observeForever(observer)
-        verify(observer).onChanged(dummyCourses)
+        verify(observer).onChanged(expected.value)
+
+        val expectedValue = expected.value
+        val actualValue = viewModel.getCourses().value
+        assertEquals(expectedValue, actualValue)
+        assertEquals(expectedValue?.data, actualValue?.data)
+        assertEquals(expectedValue?.data?.size, actualValue?.data?.size)
+    }
+
+    @Test
+    fun `getCourses should be success but data is empty`() {
+        val courses = PagedTestDataSources.snapshot()
+        val expected = MutableLiveData<Resource<PagedList<CourseEntity>>>()
+        expected.value = Resource.success(courses)
+
+        `when`(academyRepository.getAllCourses()).thenReturn(expected)
+
+        viewModel.getCourses().observeForever(observer)
+        verify(observer).onChanged(expected.value)
+
+        val actualValueDataSize = viewModel.getCourses().value?.data?.size
+        assertTrue("size of data should be 0, actual is $actualValueDataSize", actualValueDataSize == 0)
+    }
+
+    @Test
+    fun `getCourses should be error`() {
+        val expectedMessage = "Something happen dude!"
+        val expected = MutableLiveData<Resource<PagedList<CourseEntity>>>()
+        expected.value = Resource.error(expectedMessage, null)
+
+        `when`(academyRepository.getAllCourses()).thenReturn(expected)
+
+        viewModel.getCourses().observeForever(observer)
+        verify(observer).onChanged(expected.value)
+
+        val actualMessage = viewModel.getCourses().value?.message
+        assertEquals(expectedMessage, actualMessage)
+    }
+
+    class PagedTestDataSources private constructor(private val items: List<CourseEntity>) : PositionalDataSource<CourseEntity>() {
+        companion object {
+            fun snapshot(items: List<CourseEntity> = listOf()): PagedList<CourseEntity> {
+                return PagedList.Builder(PagedTestDataSources(items), 10)
+                        .setNotifyExecutor(Executors.newSingleThreadExecutor())
+                        .setFetchExecutor(Executors.newSingleThreadExecutor())
+                        .build()
+            }
+        }
+
+        override fun loadInitial(params: LoadInitialParams, callback: LoadInitialCallback<CourseEntity>) {
+            callback.onResult(items, 0, items.size)
+        }
+
+        override fun loadRange(params: LoadRangeParams, callback: LoadRangeCallback<CourseEntity>) {
+            val start = params.startPosition
+            val end = params.startPosition + params.loadSize
+            callback.onResult(items.subList(start, end))
+        }
     }
 }
--- Project Academy/12. Pagination-test/kotlin/app/src/test/java/com/dicoding/academies/ui/bookmark/BookmarkViewModelTest.kt
+++ Project Academy/12. Pagination-test/kotlin/app/src/test/java/com/dicoding/academies/ui/bookmark/BookmarkViewModelTest.kt
@@ -4,10 +4,12 @@ import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.paging.PagedList
+import androidx.paging.PositionalDataSource
 import com.dicoding.academies.data.AcademyRepository
 import com.dicoding.academies.data.source.local.entity.CourseEntity
+import com.dicoding.academies.utils.DataDummy
+import org.junit.Assert
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertNotNull
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -16,6 +18,7 @@ import org.mockito.Mock
 import org.mockito.Mockito.`when`
 import org.mockito.Mockito.verify
 import org.mockito.junit.MockitoJUnitRunner
+import java.util.concurrent.Executors
 
 @RunWith(MockitoJUnitRunner::class)
 class BookmarkViewModelTest {
@@ -30,28 +33,60 @@ class BookmarkViewModelTest {
     @Mock
     private lateinit var observer: Observer<PagedList<CourseEntity>>
 
-    @Mock
-    private lateinit var pagedList: PagedList<CourseEntity>
-
     @Before
     fun setUp() {
         viewModel = BookmarkViewModel(academyRepository)
     }
 
     @Test
-    fun getBookmark() {
-        val dummyCourses = pagedList
-        `when`(dummyCourses.size).thenReturn(5)
-        val courses = MutableLiveData<PagedList<CourseEntity>>()
-        courses.value = dummyCourses
+    fun `getBookmarkedCourses should be success`() {
+        val expected = MutableLiveData<PagedList<CourseEntity>>()
+        expected.value = PagedTestDataSources.snapshot(DataDummy.generateDummyCourses())
 
-        `when`(academyRepository.getBookmarkedCourses()).thenReturn(courses)
-        val courseEntities = viewModel.getBookmarks().value
-        verify(academyRepository).getBookmarkedCourses()
-        assertNotNull(courseEntities)
-        assertEquals(5, courseEntities?.size)
+        `when`(academyRepository.getBookmarkedCourses()).thenReturn(expected)
 
         viewModel.getBookmarks().observeForever(observer)
-        verify(observer).onChanged(dummyCourses)
+        verify(observer).onChanged(expected.value)
+
+        val expectedValue = expected.value
+        val actualValue = viewModel.getBookmarks().value
+        assertEquals(expectedValue, actualValue)
+        assertEquals(expectedValue?.snapshot(), actualValue?.snapshot())
+        assertEquals(expectedValue?.size, actualValue?.size)
+    }
+
+    @Test
+    fun `getBookmarkedCourses should be success but data is empty`() {
+        val expected = MutableLiveData<PagedList<CourseEntity>>()
+        expected.value = PagedTestDataSources.snapshot()
+
+        `when`(academyRepository.getBookmarkedCourses()).thenReturn(expected)
+
+        viewModel.getBookmarks().observeForever(observer)
+        verify(observer).onChanged(expected.value)
+
+        val actualValueDataSize = viewModel.getBookmarks().value?.size
+        Assert.assertTrue("size of data should be 0, actual is $actualValueDataSize", actualValueDataSize == 0)
+    }
+
+    class PagedTestDataSources private constructor(private val items: List<CourseEntity>) : PositionalDataSource<CourseEntity>() {
+        companion object {
+            fun snapshot(items: List<CourseEntity> = listOf()): PagedList<CourseEntity> {
+                return PagedList.Builder(PagedTestDataSources(items), 10)
+                        .setNotifyExecutor(Executors.newSingleThreadExecutor())
+                        .setFetchExecutor(Executors.newSingleThreadExecutor())
+                        .build()
+            }
+        }
+
+        override fun loadInitial(params: LoadInitialParams, callback: LoadInitialCallback<CourseEntity>) {
+            callback.onResult(items, 0, items.size)
+        }
+
+        override fun loadRange(params: LoadRangeParams, callback: LoadRangeCallback<CourseEntity>) {
+            val start = params.startPosition
+            val end = params.startPosition + params.loadSize
+            callback.onResult(items.subList(start, end))
+        }
     }
 }

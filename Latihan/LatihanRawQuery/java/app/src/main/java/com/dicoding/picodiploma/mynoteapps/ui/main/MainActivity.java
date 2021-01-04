b9df85d603f2eae4c2ea701e596d7838cf767944
Alfian Yusuf Abdullah <alfianyabdullah@gmail.com>
2021-01-04 16:58:32 +0800

    [java] implement viewBinding

--- Latihan/LatihanRawQuery/java/app/src/main/java/com/dicoding/picodiploma/mynoteapps/ui/main/MainActivity.java
+++ Latihan/LatihanRawQuery/java/app/src/main/java/com/dicoding/picodiploma/mynoteapps/ui/main/MainActivity.java
@@ -4,57 +4,52 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.paging.PagedList;
 import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.dicoding.picodiploma.mynoteapps.R;
 import com.dicoding.picodiploma.mynoteapps.database.Note;
+import com.dicoding.picodiploma.mynoteapps.databinding.ActivityMainBinding;
 import com.dicoding.picodiploma.mynoteapps.helper.SortUtils;
 import com.dicoding.picodiploma.mynoteapps.helper.ViewModelFactory;
 import com.dicoding.picodiploma.mynoteapps.ui.insert.NoteAddUpdateActivity;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import static com.dicoding.picodiploma.mynoteapps.ui.insert.NoteAddUpdateActivity.REQUEST_UPDATE;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RecyclerView recyclerView;
-
     private NotePagedListAdapter adapter;
     private MainViewModel mainViewModel;
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        mainViewModel = obtainViewModel(MainActivity.this);
 
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        mainViewModel = obtainViewModel(MainActivity.this);
         mainViewModel.getAllNotes(SortUtils.NEWEST).observe(this, noteObserver);
 
         adapter = new NotePagedListAdapter(MainActivity.this);
 
-        recyclerView = findViewById(R.id.rv_notes);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setAdapter(adapter);
+        binding.rvNotes.setLayoutManager(new LinearLayoutManager(this));
+        binding.rvNotes.setHasFixedSize(true);
+        binding.rvNotes.setAdapter(adapter);
 
-        FloatingActionButton fabAdd = findViewById(R.id.fab_add);
-        fabAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (view.getId() == R.id.fab_add) {
-                    Intent intent = new Intent(MainActivity.this, NoteAddUpdateActivity.class);
-                    startActivityForResult(intent, NoteAddUpdateActivity.REQUEST_ADD);
-                }
+        binding.fabAdd.setOnClickListener(view -> {
+            if (view.getId() == R.id.fab_add) {
+                Intent intent = new Intent(MainActivity.this, NoteAddUpdateActivity.class);
+                startActivityForResult(intent, NoteAddUpdateActivity.REQUEST_ADD);
             }
         });
     }
@@ -89,11 +84,11 @@ public class MainActivity extends AppCompatActivity {
     @NonNull
     private static MainViewModel obtainViewModel(AppCompatActivity activity) {
         ViewModelFactory factory = ViewModelFactory.getInstance(activity.getApplication());
-        return ViewModelProviders.of(activity, factory).get(MainViewModel.class);
+        return new ViewModelProvider(activity, factory).get(MainViewModel.class);
     }
 
     private void showSnackbarMessage(String message) {
-        Snackbar.make(recyclerView, message, Snackbar.LENGTH_SHORT).show();
+        Snackbar.make(binding.rvNotes, message, Snackbar.LENGTH_SHORT).show();
     }
 
     @Override
@@ -105,19 +100,21 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         String sort = SortUtils.NEWEST;
-        switch (item.getItemId()) {
-            case R.id.action_newest:
-                sort = SortUtils.NEWEST;
-                break;
-            case R.id.action_oldest:
-                sort = SortUtils.OLDEST;
-                break;
-            case R.id.action_random:
-                sort = SortUtils.RANDOM;
-                break;
-        }
+        if (item.getItemId() == R.id.action_newest)
+            sort = SortUtils.NEWEST;
+        else if (item.getItemId() == R.id.action_oldest)
+            sort = SortUtils.OLDEST;
+        else if (item.getItemId() == R.id.action_random)
+            sort = SortUtils.RANDOM;
+
         mainViewModel.getAllNotes(sort).observe(this, noteObserver);
         item.setChecked(true);
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        binding = null;
     }
 }

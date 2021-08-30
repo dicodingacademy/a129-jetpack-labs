493795098135c60661f1b5eca01e789e13593a3d
Ahmad Arif Faizin <faizin.arief@gmail.com>
2021-08-30 16:50:56 +0700

    Update Gradle 7 & clean up

--- Latihan/LatihanInstrumentTest/kotlin/app/src/androidTest/java/com/dicoding/picodiploma/myunittest/MainActivityTest.kt
+++ Latihan/LatihanInstrumentTest/kotlin/app/src/androidTest/java/com/dicoding/picodiploma/myunittest/MainActivityTest.kt
@@ -5,10 +5,8 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
-import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
